af58c03b1be8dd896b16628ff372948d15a4f41e
Emil Ohlsson <kotte@kottland.net>
2022-11-14 22:14:51 +0100

    Create proper window for contributors

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Tool for inspecting a chunk of lines and see which people are the to
 contributors according to git 
 
 ## Usage
-Select block of text and run `:Contribors`.
+Select block of text and run `:Contributors`. By default this will take entire
+file
 
 This can also be `require()` loaded from lua, and then you can pass commands to
-the tol 
+the tool 
--- lua/contributors/init.lua
+++ lua/contributors/init.lua
@@ -1,45 +1,102 @@
 local M = {}
 
+
+local function create_window()
+    vim.api.nvim_command('botright vnew')
+    local win = vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_get_current_buf()
+
+    vim.api.nvim_buf_set_name(buf, 'Contributors')
+    vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+    vim.api.nvim_buf_set_option(buf, 'swapfile', false)
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'nvim-contributors')
+    vim.api.nvim_win_set_option(win, 'wrap', false)
+    vim.api.nvim_win_set_option(win, 'cursorline', true)
+    vim.api.nvim_win_set_option(win, 'relativenumber', false)
+    vim.api.nvim_win_set_option(win, 'number', false)
+    vim.api.nvim_win_set_width(win, 35)
+
+    return win, buf
+end
+
+-- Run git blame on line range, and return a list of contributors
+-- with line score and contact address for each contributor
+local function get_contributors(file, l_start, l_end)
+    local result = {}
+    local command = { 'sh', '-c',
+        string.format([[git blame -L%s,%s --line-porcelain -- %s | \
+                        grep -e "^author-mail" | sort | uniq -c | sort -rh]],
+            l_start, l_end, file) }
+
+    -- Launch command, and populate result based on stdout output
+    local id = vim.fn.jobstart(command,
+        {
+            -- Parse produced outptut, and store in result list
+            on_stdout = function(_, output, _)
+                for _, line in ipairs(output) do
+                    local score, mail = string.match(line, "^%s*(%d+) author%-mail <(%g+)>")
+                    if score and mail then
+                        table.insert(result, { score = tonumber(score), contact = mail })
+                    end
+                end
+            end,
+            on_exit = function(_, err, _)
+                if err ~= 0 then
+                    print('There was an error running git blame')
+                end
+            end,
+            -- Unless there is some mistake, output should be small,
+            -- so might as well await all output to simplify parsing
+            stdout_buffered = true,
+            stderr_buffered = true,
+        })
+    -- If we didn't get any result within 3 seconds,
+    -- then there was probably some error
+    vim.fn.jobwait({ id }, 3000)
+    return result
+end
+
+-- Fill buffer with contributors
+local function display_contributors(buf, contributors)
+    -- Temporarily allow modification
+    vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+
+    -- Transform contribtuors to something presentable
+    local contrib_lines = {}
+    for _, contrib in ipairs(contributors) do
+        table.insert(contrib_lines, string.format("%s: %s", contrib.score, contrib.contact))
+    end
+
+    -- Clear, and write lines to buffer, and set as non-modifiable again
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+    vim.api.nvim_buf_set_lines(buf, 0, #contrib_lines, false, contrib_lines)
+    vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+end
+
 function M.setup(config)
     -- Note, this would be kind of nice to be able to run as a command
     -- that takes a movement, but this doesn't seem to be supported by
     -- Lua at the moment
-    vim.api.nvim_create_user_command('Contribtors',
+    vim.api.nvim_create_user_command('Contributors',
         function(opts)
-            local filename = vim.api.nvim_buf_get_name(0)
-            local command = { 'sh', '-c',
-                string.format([[git blame -L%s,%s --line-porcelain -- %s | \
-                                grep -e "^author-mail" | sort | uniq -c | sort -rh]],
-                    opts.line1, opts.line2, filename) }
-            vim.fn.jobstart(command,
-                {
-                    on_stdout = function(_, output, _)
-                        local scratch_buffer = vim.api.nvim_create_buf(false, true)
-                        vim.api.nvim_buf_set_lines(scratch_buffer, 0, #output, false, output)
-                        vim.api.nvim_open_win(scratch_buffer, false, {
-                            relative = 'win',
-                            width = 50,
-                            height = #output,
-                            row = 10,
-                            col = 50,
-                        })
-                    end,
-                    on_stderr = function(_, output, _)
-                        if config.debug then
-                            for _, line in ipairs(output) do
-                                print(line)
-                            end
-                        end
-                    end,
-                    on_exit = function(_, err, _)
-                        if err ~= 0 then
-                            print('There was an error running git blame')
-                        end
-                    end,
-                    stdout_buffered = true,
-                    stderr_buffered = true,
-                })
+            local file_name = vim.api.nvim_buf_get_name(0)
+
+            -- Make sure there is a focues window and associated buffer
+            -- to present contributors in. Also, file name must be read
+            -- befor shifting focus, because we don't care about the
+            -- contributor buffer itself
+            if M.win and vim.api.nvim_win_is_valid(M.win) then
+                vim.api.nvim_set_current_win(M.win)
+            else
+                M.win, M.buf = create_window()
+            end
+
+            -- Retrieve contributors for the range, and display list
+            local contributors = get_contributors(file_name, opts.line1, opts.line2)
+            display_contributors(M.buf, contributors)
         end, {
+        -- Allow range input, and by default, take entire file
         range = '%',
     })
 end
